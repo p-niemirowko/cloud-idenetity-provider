773e331b7bba7fc37ac993f6896bf94d5c709651
Przemek Niemirowko <przemyslaw.niemirowko@gmail.com>
2026-02-17 13:32:15 +0100

    Init commit

---
+++ src/main/java/com/pniemirowko/cloud/identity/provider/CloudIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.pniemirowko.cloud.identity.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudIdentityProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudIdentityProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pniemirowko/cloud/identity/provider/CloudIdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pniemirowko.cloud.identity.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudIdentityProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
